6f780414ff3a5f339344c6443661008c99b17b40
TheSilhouette <thesilhouette1705@tutanota.com>
2022-06-18 20:53:42 +0800

    testing setup and make parseFile more robust
    - used file system mocker afero instead of std
    - used logger library logrus
    - used goblin testing framework
    - parseFile now returns an error object instead of exiting the program

--- main.go
+++ main.go
@@ -1,19 +1,38 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/fs"
+	"io/ioutil"
 	"os"
 	"path"
 	"strings"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/afero"
 )
 
+var AppFs = afero.NewOsFs()
+
 func main() {
+	// set log level - can be configured with command line arguments
+	log.SetFormatter(&log.TextFormatter{
+		ForceColors:            true,
+		DisableLevelTruncation: true,
+		PadLevelText:           true,
+		DisableTimestamp:       true,
+	})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(log.DebugLevel)
+
 	const FILE_NAME = "./test"
-	var bytes, err = os.ReadFile(FILE_NAME)
+	var file, err = AppFs.Open(FILE_NAME)
 	exitIf(err, "Cannot read "+FILE_NAME)
-	var text = string(bytes)
-	parseFile(text)
+	var text, readErr = ioutil.ReadAll(file)
+	exitIf(readErr, "The file exists, but the program cannot read it")
+	var _, pathErr = parseFile(string(text))
+	exitIf(pathErr, "Some paths in the bookmark file do not exist in your file system! If you want to turn off validation, use --no-validate-path or -P")
 	fmt.Println("Bookmarks has all been generated")
 }
 
@@ -29,18 +48,34 @@ must be of format:
 if the second one ends with a trailing /, then it will be a folder bookmark instead of a file bookmark
 c ~/.config/ (will be a folder bookmark)
 ac ~/.config/alacritty/alacritty.yml (will be a file bookmark)
+
+the function also ignores lines with only comment, blank lines and any thing that comes after the format
 */
-func parseFile(text string) []Bookmark {
+var parseFile = func(text string) ([]Bookmark, error) {
 	var lines = strings.Split(text, "\n")
-	var bookmarks = make([]Bookmark, len(lines))
+	// generate 10 lines first
+	var bookmarks = make([]Bookmark, 0, 10)
 	for index, line := range lines {
-		fmt.Println("parsing line", index, ":", line)
+		log.Debugln("parsing line", index, ":", line)
+		// leave early if it is just a comment or blank lines
+		if strings.HasPrefix(line, "#") || len(strings.Trim(line, " \t\n")) == 0 {
+			continue
+		}
 		var tokens = strings.Split(line, " ")
 		// discard anything after the second token
 		var firstTwoTokens = tokens[:2]
-		var abbreviation, filepath = firstTwoTokens[0], firstTwoTokens[1]
+		var abbreviation, filepath = strings.Trim(firstTwoTokens[0], " \t"), firstTwoTokens[1]
+
+		// check if abbreviation makes sense
+		if len(abbreviation) == 0 {
+			return nil, errors.New("abbreviation is empty")
+		}
+
+		// check filpath
 		var info, err = obtainPathInfo(filepath)
-		exitIf(err, "Path"+filepath+"does not exist in your file system! If you want to turn off validation, use --no-validate-path or -P")
+		if err != nil {
+			return nil, errors.New("filepath " + filepath + " does not exist")
+		}
 		// go does not have ternaries. However, since more types may be available, this construct is okay
 		var typ string
 		if info.IsDir() {
@@ -55,12 +90,12 @@ func parseFile(text string) []Bookmark {
 			abbreviation: abbreviation,
 		}
 		bookmarks = append(bookmarks, bookmark)
-		fmt.Println("bookmark", index, ":", bookmark)
+		log.Debugln("bookmark", index, ":", bookmark)
 	}
-	return bookmarks
+	return bookmarks, nil
 }
 
-func obtainPathInfo(filepath string) (fs.FileInfo, error) {
+var obtainPathInfo = func(filepath string) (fs.FileInfo, error) {
 	var finalPath string
 	// if it is an absolute path
 	if strings.HasPrefix(filepath, "/") {
@@ -70,10 +105,10 @@ func obtainPathInfo(filepath string) (fs.FileInfo, error) {
 		exitIf(err, "For some reason, the home directory cannot be retrieved. You can explicitly provide a home path by either specifying $HOME or --home-path=<path>")
 		finalPath = path.Join(homeDir, filepath)
 	}
-	return os.Stat(finalPath)
+	return AppFs.Stat(finalPath)
 }
 
-func exitIf(err error, message string) {
+var exitIf = func(err error, message string) {
 	if err != nil {
 		fmt.Println(message)
 		os.Exit(1)
---
+++ parse_test.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"errors"
+	"os"
+	"path"
+	"testing"
+
+	. "github.com/franela/goblin"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/afero"
+)
+
+func addHome(filepath string) string {
+	var dir, err = os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+	return path.Join(dir, filepath)
+}
+
+func Test(t *testing.T) {
+	var g = Goblin(t)
+
+	g.Describe("parseFile works", func() {
+		g.Before(func() {
+			AppFs = afero.NewMemMapFs()
+
+			AppFs.Mkdir(addHome(".config"), os.ModeDir)
+			AppFs.Mkdir(addHome(".config/whatever"), os.ModeDir)
+			AppFs.Create(addHome(".config/whatever/conf"))
+			log.SetLevel(log.FatalLevel)
+		})
+
+		var neededBookmarks = []Bookmark{
+			{typ: "dir", path: ".config/", abbreviation: "c"},
+			{typ: "file", path: ".config/whatever/conf", abbreviation: "cw"},
+		}
+
+		g.It("parse valid strings that have no comments", func() {
+
+			var res = []struct {
+				in   string
+				want []Bookmark
+			}{
+				{
+					in:   "c .config/",
+					want: []Bookmark{neededBookmarks[0]},
+				},
+				{
+					in:   "cw .config/whatever/conf",
+					want: []Bookmark{neededBookmarks[1]},
+				},
+				{
+					in:   "c .config/\ncw .config/whatever/conf",
+					want: neededBookmarks,
+				},
+			}
+
+			for _, pair := range res {
+				var out, _ = parseFile(pair.in)
+				g.Assert(out).Equal(pair.want)
+			}
+		})
+
+		g.It("parse valid strings with comments and new lines", func() {
+			var res = []struct {
+				in   string
+				want []Bookmark
+			}{
+				{
+					in:   "#first line is comment\nc .config/",
+					want: []Bookmark{neededBookmarks[0]},
+				},
+				{
+					in:   "\ncw .config/whatever/conf\n\n#last line is a comment",
+					want: []Bookmark{neededBookmarks[1]},
+				},
+				{
+					in:   "#hi\nc .config/\ncw .config/whatever/conf # between lines\n",
+					want: neededBookmarks,
+				},
+			}
+
+			for _, pair := range res {
+				var out, _ = parseFile(pair.in)
+				g.Assert(out).Equal(pair.want)
+			}
+		})
+
+		g.It("can return abbreviation error and filepath error", func() {
+			var res = []struct {
+				in   string
+				want error
+			}{
+				{
+					in:   " /varr",
+					want: errors.New("abbreviation is empty"),
+				},
+				{
+					in:   "v /varr",
+					want: errors.New("filepath /varr does not exist"),
+				},
+				{
+					in:   "cj .config/jj",
+					want: errors.New("filepath .config/jj does not exist"),
+				},
+			}
+
+			for _, pair := range res {
+				var _, err = parseFile(pair.in)
+				g.Assert(err).Equal(pair.want)
+			}
+		})
+	})
+
+}
